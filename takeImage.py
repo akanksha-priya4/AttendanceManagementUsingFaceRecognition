19ba23bfe2250dffd7b62c2b306e0b55dc8c6c40
akanksha-priya4 <priyaakanksha393@gmail.com>
2025-03-26 10:25:00 +0530

    Optimize database queries

--- takeImage.py
+++ takeImage.py
@@ -1,3 +1,4 @@
+# Updated on 2025-03-26 - commit 2
 # Updated on 2025-03-04 - commit 2
 # Updated on 2024-03-16 - commit 3
 # Updated on 2024-03-16 - commit 2
@@ -77,6 +78,7 @@ def TakeImage(l1, l2, haarcasecade_path, trainimage_path, message, err_screen,te
         except FileExistsError as F:
             F = "Student Data already exists"
             text_to_speech(F)
+
 
 
 
